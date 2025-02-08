2dd4786cc81e84f4af83443ff7cd3e4e98004caf
Niketan goyal <niketangoyal3@gmail.com>
2025-02-08 13:30:37 +0530

    initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require('../models/userModel');
+
+// Get all users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Create a user
+const createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = { getUsers, createUser };
