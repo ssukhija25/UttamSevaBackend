72756fdb5981e466d06de3e6a93d69fc08d409d3
vishal123k <vishalk38916@gamil.com>
2025-05-09 08:59:06 +0530

    latest update..

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 
 // Create Razorpay Order & Save Payment
-router.post('/', paymentController.createPayment);
+router.post('/createpayment', paymentController.createPayment);
 
 // Verify Razorpay Payment
 router.post('/verify', paymentController.verifyPayment);
 
 // Admin - Get All Payments
-router.get('/', paymentController.getAllPayments);
+router.get('/allpayment', paymentController.getAllPayments);
 
 // Get User Payments
 router.get('/user/:userId', paymentController.getPaymentsByUser);
