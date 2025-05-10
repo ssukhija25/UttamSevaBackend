9d58bc80505e8c1e6f0e9b5c34a67deff77e52b8
Niketan goyal <niketangoyal3@gmail.com>
2025-05-10 13:10:43 +0530

    otp controller generate and verify otp

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 app.use("/api/mainpage", mainRoute);
 app.use('/api/otp', otpRoutes);
-
+app.use('/',(req,res)=>{  res.status(200).json({ success: true, message: "API endpoint not found" });
+})
 
 app.use((req, res) => {
   res.status(404).json({ success: false, message: "API endpoint not found" });
--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const otpController = require('../controllers/otpController');
 
-router.post('/send-otp', otpController.sendOtp);
+router.post('/generate-otp', otpController.sendOtp);
 router.post('/verify-otp', otpController.verifyOtp);
 
 module.exports = router;
